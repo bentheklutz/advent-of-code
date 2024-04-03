2c6e0abdd779fd5d0a31ed6c6f8df9eae4caf507
Benjamin Hetz <bwhdev@gmail.com>
2024-04-03 03:52:33 -0500

    day 6 parts 1 and 2 because i'm like that

---
+++ 2023/06/src/01.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include "util.h"
+
+const char *default_path = "input/06.txt";
+const char *example_path = "input/example.txt";
+
+#ifdef SILENT
+#define fprintf(fd, fmt, ...) do { } while (0)
+#endif
+
+const char *determine_input_path(int argc, char **argv) {
+	const char *input_path = default_path;
+	if (argc > 1) {
+		if (strncmp(argv[1], "-e", 2) == 0 ||
+		    strncmp(argv[1], "--example", 9) == 0) {
+			input_path = example_path;
+		}
+		if (strncmp(argv[1], "-f", 2) == 0 ||
+		    strncmp(argv[1], "--file", 6) == 0) {
+			if (argc == 2) {
+				fprintf(stderr, "Option -f <file> requires an argument, none provided\n");
+				exit(1);
+			}
+			input_path = argv[2];
+		}
+	}
+	return input_path;
+}
+
+long calculate_maximum_distance(long race_time) {
+	long max_distance = 0;
+
+	for (long hold_time = 0; hold_time < race_time; hold_time += 1) {
+		// d = v * t
+		// v = hold_time * 1 mm/ms
+		// t = race_time - hold_time
+		long distance = (race_time - hold_time)*hold_time;
+		if (distance > max_distance) { max_distance = distance; }
+	}
+
+	return max_distance;
+}
+
+int main(int argc, char **argv) {
+	const char *input_path = determine_input_path(argc, argv);
+	StringBuffer file;
+	if (!read_entire_file(input_path, &file)) { return 1; }
+
+	StringView content = {
+		.data = file.data,
+		.count = file.count,
+	};
+
+	DynArray lines = string_view_lines(content);
+	StringView *lines_arr = dyn_array_as_type(lines, StringView);
+
+	StringView time_line = lines_arr[0];
+	StringView dist_line = lines_arr[1];
+
+	string_view_split_on(&time_line, ':');
+	DynArray times_da = string_view_parse_numbers(time_line);
+
+	string_view_split_on(&dist_line, ':');
+	DynArray dists_da = string_view_parse_numbers(dist_line);
+
+	if (times_da.count != dists_da.count) {
+		fprintf(stderr, "Different number of times (%ld) and distances (%ld). This input makes no fuckin sense.\n", times_da.count, dists_da.count);
+		return 1;
+	}
+
+	size_t races = times_da.count;
+	long *times = dyn_array_as_type(times_da, long);
+	long *dists = dyn_array_as_type(dists_da, long);
+
+	long answer = 1;
+	for (size_t race = 0; race < races; race += 1) {
+		long race_time = times[race];
+		long race_record = dists[race];
+		int ways_to_win = 0;
+		for (long hold_time = 0; hold_time < race_time; hold_time += 1) {
+			long distance = hold_time*(race_time-hold_time);
+			if (distance > race_record) {
+				ways_to_win += 1;
+			}
+		}
+		answer *= ways_to_win;
+	}
+
+	printf("The answer is %ld.\n", answer);
+
+	return 0;
+}
---
+++ 2023/06/src/util.h
@@ -0,0 +1,311 @@
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <ctype.h>
+
+typedef struct StringBuffer {
+	char *data;
+	size_t count;
+	size_t capacity;
+} StringBuffer;
+
+typedef struct StringView {
+	char *data;
+	size_t count;
+} StringView;
+
+#define SV_FMT "%.*s"
+#define SV_ARG(sv) (int)((sv).count), (sv).data
+typedef struct DynArray {
+	void   *data;
+	size_t count;
+	size_t capacity;
+} DynArray;
+
+#define DYNARRAY_INIT_CAP 1024
+
+#define dyn_array_as_type(da, T) ((T *)(da).data)
+
+#define dyn_array_add(da, item)                                                    \
+	{                                                                             \
+		if ((da)->count == (da)->capacity) {                                      \
+			(da)->capacity = (da)->capacity ?                                     \
+				(da)->capacity*2                                                  \
+				: DYNARRAY_INIT_CAP;                                              \
+			(da)->data = realloc((da)->data, (da)->capacity*sizeof(item));                     \
+		}                                                                         \
+		memcpy((char *)(da)->data+(da)->count*sizeof(item), &item, sizeof(item)); \
+		(da)->count += 1;                                                         \
+	}
+
+bool read_entire_file(const char *file_path, StringBuffer *buf) {
+	bool result = false;
+
+	if (!buf) { return result; }
+
+	int fd = open(file_path, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Failed to open file: %s\n", file_path);
+		return result;
+	}
+
+	struct stat st = {0};
+	fstat(fd, &st);
+
+	size_t file_size = st.st_size;
+
+	char *contents_buffer = malloc(file_size);
+	if (!contents_buffer) {
+		fprintf(stderr, "Failed to allocate %zu bytes for file contents\n", file_size);
+		close(fd);
+		return result;
+	}
+	char read_buffer[4096];
+
+	for(int i = 0; ; ++i) {
+		int bytes_read = read(fd, read_buffer, sizeof(read_buffer));
+		if (!bytes_read) { break; }
+		memcpy(contents_buffer+(i*sizeof(read_buffer)), read_buffer, bytes_read);
+	}
+
+	buf->data = contents_buffer;
+	buf->count = file_size;
+	buf->capacity = file_size;
+
+	close(fd);
+
+	result = true;
+	return result;
+}
+
+StringView string_view_trim_left(StringView str) {
+	StringView result = {0};
+	if (!str.data) { return result; }
+
+	result.data = str.data;
+	result.count = str.count;
+
+	while (result.count > 0 && isspace(*result.data)) {
+		result.data  += 1;
+		result.count -= 1;
+	}
+
+	if (!result.count) {
+		result.data = 0;
+	}
+	return result;
+}
+
+StringView string_view_take_line(StringView *str) {
+	StringView line = {0};
+
+	if (!str || !str->data || !str->count) { return line; }
+
+	line.data = str->data;
+
+	bool had_carriage_return = false;
+	for (size_t i = 0; i < str->count; ++i) {
+		if (line.data[i] == '\n') {
+			if (i && line.data[i-1] == '\r') { had_carriage_return = true; }
+			break;
+		}
+		line.count += 1;
+	}
+
+	// Skip past the matched newline if it exists
+	if (line.count < str->count) {
+		str->data += line.count + 1;
+		str->count -= line.count + 1;
+	} else {
+		str->data  += line.count;
+		str->count -= line.count;
+	}
+
+	if (had_carriage_return) { line.count -= 1; }
+
+	return line;
+}
+
+StringView string_view_split_on(StringView *str, char delimiter) {
+	StringView result = {0};
+
+	if (!str || !str->data || !str->count) { return result; }
+
+	result.data = str->data;
+
+	for (size_t i = 0; i < str->count; ++i) {
+		if (result.data[i] == delimiter) {
+			break;
+		}
+		result.count += 1;
+	}
+
+	if (result.count < str->count) {
+		str->data += result.count + 1;
+		str->count -= result.count + 1;
+	} else {
+		str->data  += result.count;
+		str->count -= result.count;
+	}
+
+	return result;
+}
+
+void string_view_eat(StringView *str, char character) {
+	if (!str || !str->data || !str->count) { return; }
+
+	for (size_t i = 0; i < str->count; ++i) {
+		if (*(str->data) != character) { break; }
+		str->count -= 1;
+		str->data += 1;
+	}
+}
+
+DynArray string_view_lines(StringView str) {
+	DynArray arr = {0};
+	for (int i = 0; str.count > 0; ++i) {
+		StringView line = string_view_take_line(&str);
+		dyn_array_add(&arr, line);
+	}
+	return arr;
+}
+
+DynArray string_view_take_numbers(StringView str) {
+	DynArray arr = {0};
+	StringView current = {0};
+	bool in_number = false;
+	for (int i = 0; str.count > 0; ++i) {
+		if (in_number) {
+			if (str.data[0] >= '0' && str.data[0] <= '9') {
+				current.count += 1;
+			} else {
+				dyn_array_add(&arr, current);
+				current.data = 0x0;
+				current.count = 0;
+				in_number = false;
+			}
+		} else {
+			if (str.data[0] >= '0' && str.data[0] <= '9') {
+				in_number = true;
+				current.data = str.data;
+				current.count += 1;
+			}
+		}
+		str.data  += 1;
+		str.count -= 1;
+	}
+	if (in_number) {
+		dyn_array_add(&arr, current);
+	}
+	return arr;
+}
+
+DynArray string_view_parse_numbers(StringView str) {
+	DynArray arr = {0};
+	StringView current = {0};
+	bool in_number = false;
+	for (int i = 0; str.count > 0; ++i) {
+		if (in_number) {
+			if (str.data[0] >= '0' && str.data[0] <= '9') {
+				current.count += 1;
+			} else {
+				long number = strtoll(current.data, 0x0, 10);
+				dyn_array_add(&arr, number);
+				current.data = 0x0;
+				current.count = 0;
+				in_number = false;
+			}
+		} else {
+			if (str.data[0] >= '0' && str.data[0] <= '9') {
+				in_number = true;
+				current.data = str.data;
+				current.count += 1;
+			}
+		}
+		str.data  += 1;
+		str.count -= 1;
+	}
+	if (in_number) {
+		long number = strtoll(current.data, 0x0, 10);
+		dyn_array_add(&arr, number);
+	}
+	return arr;
+}
+
+StringView string_view_remove_spaces(StringView input, int num_spaces) {
+	StringView result = {0};
+
+	result.data = malloc(input.count - num_spaces);
+	result.count = input.count - num_spaces;
+
+	size_t input_index = 0;
+	for (size_t i = 0; i < result.count; i += 1) {
+		while (input.data[input_index] == ' ') {
+			input_index += 1;
+		}
+		result.data[i] = input.data[input_index];
+		input_index += 1;
+	}
+
+	return result;
+}
+
+DynArray string_view_parse_numbers_with_spaces(StringView str) {
+	DynArray arr = {0};
+	StringView current = {0};
+	int num_spaces_in_number;
+	bool in_number = false;
+	for (int i = 0; str.count > 0; ++i) {
+		if (in_number) {
+			if (str.data[0] >= '0' && str.data[0] <= '9') {
+				current.count += 1;
+			} else if (str.data[0] == ' ') {
+				current.count += 1;
+				num_spaces_in_number += 1;
+			} else {
+				// An allocation here sucks, but I don't really want to fuck with the input.
+				// Maybe I should.
+				StringView sv = string_view_remove_spaces(current, num_spaces_in_number);
+				long number = strtoll(sv.data, 0x0, 10);
+				dyn_array_add(&arr, number);
+				current.data = 0x0;
+				current.count = 0;
+				in_number = false;
+			}
+		} else {
+			if (str.data[0] >= '0' && str.data[0] <= '9') {
+				in_number = true;
+				num_spaces_in_number = 0;
+				current.data = str.data;
+				current.count += 1;
+			}
+		}
+		str.data  += 1;
+		str.count -= 1;
+	}
+	if (in_number) {
+		StringView sv = string_view_remove_spaces(current, num_spaces_in_number);
+		long number = strtoll(sv.data, 0x0, 10);
+		dyn_array_add(&arr, number);
+	}
+	return arr;
+}
+
+DynArray string_view_take_symbols(StringView str) {
+	DynArray arr = {0};
+	StringView current = {0};
+	for (int i = 0; str.count > 0; ++i) {
+		if (str.data[0] != '.' && !(str.data[0] >= '0' && str.data[0] <= '9')) {
+			current.data = str.data;
+			current.count = 1;
+			dyn_array_add(&arr, current);
+		}
+		str.data  += 1;
+		str.count -= 1;
+	}
+	return arr;
+}
